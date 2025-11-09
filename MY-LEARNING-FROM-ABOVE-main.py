1bd4b043d2e51eba9674a105e3d4f476cdae2b77
dhairyashild <desaidhairyashil1@gmail.com>
2025-11-09 13:20:14 +0530

    Create MY-LEARNING-FROM-ABOVE-main.py

---
+++ MY-LEARNING-FROM-ABOVE-main.py
@@ -0,0 +1,66 @@
+comments == """multiline """"        /      #
+
+print(a[DEFAULTZERO:DEFAULTLAST:DEFAULT1])
+
+print(name,age)  == akarsh,23
+
+age=input("PROMPT JI DISTE")     /     int(input("hello what is your age"))
+
+# OPERATOR
+ARITHMETIC 
+print(b//a)    ==6    IT CONVERTS ABOVE O/P INTO INTEGER MEANS POINT KADTO       
+print(5**2)    ==25   VARG
+print(32%5)    ==2    BAKI 
+
+ASSIGNMENT OP
+a += 30     AND  a=a+30   BOTH SAME
+
+COMPARISON OP
+print(a == b)      == TRUE
+print(a != b)    == FALSE
+
+#IF ELIF ELSE
+if condition1:
+                              # RUN Code if condition1 is True
+elif condition2:
+                              # RUN Code if condition2 is True
+else:
+                              # RUN  Code if all conditions are False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
