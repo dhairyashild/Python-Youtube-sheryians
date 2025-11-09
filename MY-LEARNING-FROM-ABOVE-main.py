789cfcb0e86797f6e831ea723b24d5bdd10dc4e4
dhairyashild <desaidhairyashil1@gmail.com>
2025-11-09 21:55:06 +0530

    Update MY-LEARNING-FROM-ABOVE-main.py

--- MY-LEARNING-FROM-ABOVE-main.py
+++ MY-LEARNING-FROM-ABOVE-main.py
@@ -4,7 +4,7 @@ print(a[DEFAULTZERO:DEFAULTLAST:DEFAULT1])
 
 print(name,age)  == akarsh,23
 
-age=input("PROMPT JI DISTE")     /     int(input("hello what is your age"))
+age=input("PROMPT JI DISTE")     /     int(input("hello what is your age")) #ERROR-int need to get number from user so math apply with them
 
 # OPERATOR
 ARITHMETIC 
@@ -20,32 +20,46 @@ print(a == b)      == TRUE
 print(a != b)    == FALSE
 
 #IF ELIF ELSE
-if condition1:
+if condition1:                                                            if value1=="m":         #ERROR- DOUBLE INVERTED FORGET TO ADD
                               # RUN Code if condition1 is True
-elif condition2:
+elif condition2:                                                         elif temp >=0 and temp <=10:
                               # RUN Code if condition2 is True
 else:
                               # RUN  Code if all conditions are False
 
 
+LOGICAL OPERATOR==
+AND ==== ALL MUST TRUE
+print(12 >20 and 123 > 100 and 34 == 34 and 45 < 90)    ==FALSE
+
+OR  ==== ANY 1 MUST TRUE
+
+------------------------------------------------------------    2 hr notes            ----------------------------------------------------------------------------------
+# LOOPS
+#FOR LOOP NOTES
+
+FUNCTION = JECHYA NANTAR () eto tyala function mhanatat eg- print() , range(), input()
+range(start,stop, step)
+
+1)#FOR LOOP WITH NUMBERS
+# PRINT -1 TO -10
+for i in range(-1,-11,-1):                            # ERROR- FORGET :     # GIVE LAST NUMBER WITH+1  # U  can use any word instead of i        
+  print(i)                                            # ONLYLAST NUMBE ALSO WORKS LIKE range(21)    # need indentation space for 2nd line  
+
+2) FOR LOOP FOR STRING
+name= " dhairya s beast "
+print(len(name))                     ========17
+
+a = "SHERYIANS TEACHES INDUSTRY THINGS"
+print(len(a))                            =====34
+for i in range(len(a)):                  ===== range(34) runs here , here no need to add +1 to stop number as len gives +1 number already
+    print(a[i])                          ===== (a[34])      = (a[start:stop:step])            
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for i in a:
+  print(i)            -----also gives full of a value but to cut a string we use string
+  
+# REMEMBER RANGE START,STOP,STEP DIVIDED BY comma ONLY AND WHILE STRING START:STOP:STEP GIVEN BY :
 
 
 
