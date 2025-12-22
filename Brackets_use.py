b746e6c7953eaa8e9341d79520aca993883ccd53
dhairyashild <desaidhairyashil1@gmail.com>
2025-12-22 19:39:54 +0530

    Create Brackets_use.py

---
+++ Brackets_use.py
@@ -0,0 +1,69 @@
+COMMENTS=  '''      '''                 """    """                  #
+
+print(type(v)) ==== both round FOR TYPE SUCH AS FLOAT , INT
+
+print(a[0:4:1]) == FOR SLICING STRING
+
+a=12                      --Anything go into string but word not go into int
+a=str/int/float/bool(a)  
+##########################################################################################      52 minutes complete
+# CALLING VAR VALUE IN PRINT
+#O/P
+i = 10                  print(var)  --sufficient to call var vale
+print(i) or print("Value:", i)      --- calling var needs nothing like ${}, just need varname here i
+print(f"{var}")  -- f"{    these 3 thing need to call MULTIPLE VAR/CALCULATIONS in string      
+
+#INPUT
+age = int(input("PROMPT JI DISTE"))     ---      int TO CONVERT STR INTO INT AS input taken by python as string only
+##########################################################################################    1 HR
+#ARITHMETIC OPERATOR
+print(b//a)    ==6    IT CONVERTS ABOVE O/P INTO INTEGER
+print(5**2)    ==25   VARG
+print(32%5)    ==2    BAKI 
+
+#COMPOUND ARITHMETIC  === REASSIGN VALUES
+a=20
+a=a+20              /  a+=20
+
+COMPARISON OPERATOS == GIVE TRUE / FALSE AS O/P  +   = symbolchya left la extra symbol lagto kayam >/</!
+print(a == b)      == TRUE
+print(a != b)    == FALSE
+print(45 <= 45) 
+
+# string comparison veli each letter chi ord value ghete ani ti kramane compare karate
+# print(ord("A"))
+# print(ord("B"))
+# print("ABC" > "ACD")
+
+LOGICAL OPERATOR==
+-AND ==== ALL MUST TRUE
+        print(12 >20 and 123 > 100 and 34 == 34 and 45 < 90)    ==FALSE
+-OR  ==== ANY 1 MUST TRUE
+        print(12 !=12 or 23 ==45 or 67 == 56 or 10 > 5)     ==TRUE
+-print(not 12 == 12)      ==AFTER not I WILL REVERSE THE O/P
+
+
+#IF-ELSE FOR GIVING PRECEDENCE OF CONDITION 1 BY ONE
+if  COND1 :                           COND1 WITH OR WORKS BUT WITH AND NOT WORKS       # if gen == 'M' or gen == 'm': WORKS BUT  # if gen == 'M' and gen == 'm':  NOT WORKS
+     COMMAND
+elif COND2  :
+     COMMAND
+else:
+     COMMAND
+if CONDITION-1 WITH O/P TRUE/FALSE AND SYMBOL ==/>=/<=/>/< ONLY =NOT WORKS WITH IF AS ITS ASSIGN VALUE NOT COMPARE :   age==15   works but age==ten not works need ""  like "ten"
+    if CONDITION-1 TRUE THEN RUN THIS
+elif CONDITION-2 :
+    if CONDITION-2 TRUE THEN RUN THIS
+else:
+    if above all contion FALSE then run this
+
+
+# if gen == 'M' or gen == 'm':
+or works but and not
+# if gen == 'M' and gen == 'm':
+
+
+
+
+
+
