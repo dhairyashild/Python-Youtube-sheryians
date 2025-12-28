6174031f775e1c021cbe8f62b618e7a433b91e9a
dhairyashild <desaidhairyashil1@gmail.com>
2025-12-28 22:37:27 +0530

    Update my-programs.py

--- my-programs.py
+++ my-programs.py
@@ -1,3 +1,3 @@
-P-1    PRINT TABLEOF 5
-for i in range(5,51,5)
+P-1    PRINT TABLEOF 5                       table of any number ==take n as i/p
+for i in range(5,51,5)                       for i in range(n,(n*10)+1,n)
   print(i)
